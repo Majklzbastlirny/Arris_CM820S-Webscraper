b224dfa8e6ee1d63273822ec865822288cd5b84f
Majklzbastlirny <minecz.michal.basler@gmail.com>
2022-05-01 16:43:13 +0200

    Made it headless

--- Scraper.py
+++ Scraper.py
@@ -31,6 +31,9 @@ else:
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(options=chrome_options)
 URL = "http://192.168.100.1/cgi-bin/status_cgi"
 driver.get(URL)
